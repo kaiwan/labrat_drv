71837085712eeae8efff66d237588a8dc3d7cc3c
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2022-11-26 18:50:42 +0530

    bash strict mode effects

--- dht2x_temp_humd_i2c_driver/disp_temp_humd.sh
+++ dht2x_temp_humd_i2c_driver/disp_temp_humd.sh
@@ -7,17 +7,17 @@ chip_addr=0038  # chip addr is 0x38; update if required
 intv_sec=1
 
 # ${VARNAME:-DEFAULT_VALUE} evals to DEFAULT_VALUE if VARNAME undefined
-help=${1:-}
-[ "$help" = "-h" ] && {
+p1=${1:-}
+[ "$p1" = "-h" ] && {
    echo "Usage: ${name} [refresh-interval-in-seconds]"
    echo "Displays:
    temperature in millidegrees Celsius,rel humidity in milli percentage
    (you need to div both values by 1000.0)"
    exit 0
 }
-[ $# -eq 1 ] && intv_sec=$1
+[ $# -eq 1 ] && intv_sec=$p1
 
-lsmod|grep -q -w ${drv}
+lsmod|grep -w "^${drv}" >/dev/null 2>&1
 [ $? -ne 0 ] && {
   echo "${name}: driver ${drv} not loaded? aborting..."
   exit 1
