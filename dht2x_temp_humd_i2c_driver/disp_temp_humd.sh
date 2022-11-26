a0c93480be0770bf5a054c9ace822cede30f761d
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2022-11-26 13:04:09 +0000

    strict bash; chk for module

--- dht2x_temp_humd_i2c_driver/disp_temp_humd.sh
+++ dht2x_temp_humd_i2c_driver/disp_temp_humd.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+set -euo pipefail
 name=$(basename $0)
+drv=dht2x_kdrv
 i2cbus=1        # on I2C bus #1; update if required
 chip_addr=0038  # chip addr is 0x38; update if required
 intv_sec=1
 
-[ "$1" = "-h" ] && {
+# ${VARNAME:-DEFAULT_VALUE} evals to DEFAULT_VALUE if VARNAME undefined
+help=${1:-}
+[ "$help" = "-h" ] && {
    echo "Usage: ${name} [refresh-interval-in-seconds]"
    echo "Displays:
    temperature in millidegrees Celsius,rel humidity in milli percentage
@@ -13,8 +17,14 @@ intv_sec=1
 }
 [ $# -eq 1 ] && intv_sec=$1
 
+lsmod|grep -q -w ${drv}
+[ $? -ne 0 ] && {
+  echo "${name}: driver ${drv} not loaded? aborting..."
+  exit 1
+}
 humd_file=/sys/bus/i2c/devices/${i2cbus}-${chip_addr}/dht2x_humd
 temp_file=/sys/bus/i2c/devices/${i2cbus}-${chip_addr}/dht2x_temp
+
 while [ true ]
 do
    temp=$(cat ${temp_file})
