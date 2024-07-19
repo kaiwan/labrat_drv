048092717d051af928a103cdc7428a414211563a
Kaiwan N Billimoria <kaiwan.billimoria@gmail.com>
2024-07-19 04:23:05 +0000

    Turn off all user LEDs as required

--- led_blink/bbb_ledblink/ledblink_bbb.sh
+++ led_blink/bbb_ledblink/ledblink_bbb.sh
@@ -3,17 +3,19 @@
 # NOTE: meant for the BeagleBone Black running standard Debian
 # ref: https://robotic-controls.com/learn/beaglebone/beaglebone-black-built-leds
 name=$(basename $0)
+
 usage() {
  echo "Usage: ${name} LED#
 FYI, defaults on the BBB are:
-    • USR0 :  [heartbeat]    (like an aircraft’s lights!)
-    • USR1 :  [mmc0]
-    • USR2 :  [cpu0]
-    • USR3 :  [mmc1]
+    USR0 :  [heartbeat]
+    USR1 :  [mmc0]
+    USR2 :  [cpu0]
+    USR3 :  [mmc1]
 (Pass LED# as 0 for USR0, 1 for USR1, ...)
 "
 }
 
+#--- 'main'
 [ $(id -u) -ne 0 ] && {
 	echo "needs root."
 	exit 1
@@ -28,9 +30,12 @@ LEDNUM=$1
 	exit 1
 }
 
+./turn_off_all_userleds
+
 # take it over
 echo timer > /sys/devices/platform/leds/leds/beaglebone\:green\:usr${LEDNUM}/trigger
 
-# blink it by toggling it on/off
+# Blink it by toggling it between on (for 100ms) and then off (for 500ms)
 echo 100 > /sys/devices/platform/leds/leds/beaglebone\:green\:usr${LEDNUM}/delay_on
 echo 500 > /sys/devices/platform/leds/leds/beaglebone\:green\:usr${LEDNUM}/delay_off
+exit 0
